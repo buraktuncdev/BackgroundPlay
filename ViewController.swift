5e9e05ee226afd65c687929784a6eece02588c51
Burak Tunc <burak.tunc@check24.de>
2021-01-17 20:29:44 +0100

    go background

--- ViewController.swift
+++ ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func findPath(){
+        do {
+            let path = Bundle.main.path(forResource: "music", ofType: ".mp3")
+            player = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: path!))
+            player.prepareToPlay()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 
-
+    @IBAction func startButtonPressed(_ sender: Any) {
+        player.play()
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        player.stop()
+    }
+    
+    
 }
 
